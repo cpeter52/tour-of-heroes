2d6f880bcaaf3542ccf816a3e8eb5565708213a0
Chris Peterson <petersonchris1216@gmail.com>
2019-03-15 13:17:34 -0400

    Update mock-heroes.ts

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Black Panther' },
+  { id: 12, name: 'Black Widow' },
+  { id: 13, name: 'Static Shock' },
+  { id: 14, name: 'The Hulk' },
+  { id: 15, name: 'Thor' },
+  { id: 16, name: 'Iron Man' },
+  { id: 17, name: 'WAR Machine' },
+  { id: 18, name: 'Mario' },
+  { id: 19, name: 'Sonic' },
+  { id: 20, name: 'SpiderMan' }
 ];
